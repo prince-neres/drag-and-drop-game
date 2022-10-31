e842f76a2208f1954043cbb37f6e959c058cbd4b
Prince Neres <prince.neres@ambar.tech>
2022-10-31 08:13:51 -0300

    Fix validation game

--- static/js/utils.js
+++ static/js/utils.js
@@ -17,3 +17,17 @@
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const count_items = (categories) => {
+  let number_of_items = 0
+  
+  categories.forEach((arr) => {
+    arr.items.map((i) => {
+      number_of_items++
+    })
+  })
+
+  return number_of_items
+}
+
+export { count_items }
