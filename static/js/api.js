e8a4d33470333e2421d10f53057c0a2a0d6fd985
Prince Neres <prince84224960@gmail.com>
2023-04-13 16:16:05 -0300

    Fix port of frontend

--- static/js/api.js
+++ static/js/api.js
@@ -1,183 +1,178 @@
-const current_base = window.location.href
-const url_base = 'http://localhost:5000/'
-
+const current_base = window.location.href;
+const url_base = "http://localhost:3000/";
 
 function criar_tema(event) {
-  event.preventDefault()
-  let name = $('#name').val()
-  let description = $('#description').val()
-  let categories = $('#groups #category-input')
-  let private = $('#private').is(':checked')
-  let tema = {}
+  event.preventDefault();
+  let name = $("#name").val();
+  let description = $("#description").val();
+  let categories = $("#groups #category-input");
+  let private = $("#private").is(":checked");
+  let tema = {};
 
-  tema.name = name
-  tema.description = description
-  tema.categories = []
+  tema.name = name;
+  tema.description = description;
+  tema.categories = [];
 
-  if(private) {
-    tema.private = true
-    tema.password = $('#password').val()
+  if (private) {
+    tema.private = true;
+    tema.password = $("#password").val();
   } else {
-    tema.private = false
+    tema.private = false;
   }
 
-  $.each(categories, function(i, l) {
-    category = {}
-    items = $(l).children('#item-input')
-    category_name = $(l).children('#category-name').val()
-    category.name = category_name
-    category.items = []
+  $.each(categories, function (i, l) {
+    category = {};
+    items = $(l).children("#item-input");
+    category_name = $(l).children("#category-name").val();
+    category.name = category_name;
+    category.items = [];
 
-    $.each(items, function(i, l) {
-      item_name = $(l).children('#item-name').val()
-      item_obj = {'name': item_name}
-      category['items'].push(item_obj)
-    })
+    $.each(items, function (i, l) {
+      item_name = $(l).children("#item-name").val();
+      item_obj = { name: item_name };
+      category["items"].push(item_obj);
+    });
 
-    tema.categories.push(category)
-  })
+    tema.categories.push(category);
+  });
 
   $.ajax({
-    type: 'POST',
+    type: "POST",
     url: `${url_base}create_theme`,
     data: JSON.stringify(tema),
-    contentType:'application/json; charset=utf-8',
-    dataType:'json',
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
     success: function (response) {
-      var alert = ''
-      
+      var alert = "";
+
       if (response.success) {
         alert = `
           <div class="alert alert-success alert-dismissible fade show" role="alert">
             <strong>${response.success}</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
-        `
-        window.location.href = `http://localhost:5000`
+        `;
+        window.location.href = `http://localhost:3000`;
       } else {
         alert = `
           <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <strong>${response.error}</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
-        `
+        `;
       }
-      $(alert).insertBefore('#formulario')
-    }
-  })
+      $(alert).insertBefore("#formulario");
+    },
+  });
 }
-
 
 function editar_tema(_id) {
-  event.preventDefault()
-  let name = $('#name').val()
-  let description = $('#description').val()
-  let categories = $('#groups #category-input')
-  let private = $('#private').is(':checked')
-  let tema = {}
+  event.preventDefault();
+  let name = $("#name").val();
+  let description = $("#description").val();
+  let categories = $("#groups #category-input");
+  let private = $("#private").is(":checked");
+  let tema = {};
 
-  tema.name = name
-  tema.description = description
-  tema.categories = []
+  tema.name = name;
+  tema.description = description;
+  tema.categories = [];
 
-  if(private) {
-    tema.private = true
-    tema.password = $('#password').val()
+  if (private) {
+    tema.private = true;
+    tema.password = $("#password").val();
   } else {
-    tema.private = false
+    tema.private = false;
   }
 
-  $.each(categories, function(i, l) {
-    category = {}
-    items = $(l).children('#item-input')
-    category_name = $(l).children('#category-name').val()
-    category.name = category_name
-    category.items = []
+  $.each(categories, function (i, l) {
+    category = {};
+    items = $(l).children("#item-input");
+    category_name = $(l).children("#category-name").val();
+    category.name = category_name;
+    category.items = [];
 
-    $.each(items, function(i, l) {
-      item_name = $(l).children('#item-name').val()
-      item_obj = { 'name': item_name }
-      category['items'].push(item_obj)
-    })
+    $.each(items, function (i, l) {
+      item_name = $(l).children("#item-name").val();
+      item_obj = { name: item_name };
+      category["items"].push(item_obj);
+    });
 
-    tema.categories.push(category)
-  })
-  
+    tema.categories.push(category);
+  });
+
   $.ajax({
-    type: 'PUT',
+    type: "PUT",
     url: `${url_base}update_theme/${_id}`,
     data: JSON.stringify(tema),
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
     success: function (response) {
-      var alert = ''
-      
+      var alert = "";
+
       if (response.success) {
         alert = `
           <div class="alert alert-success alert-dismissible fade show" role="alert">
             <strong>${response.success}</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
-        `
+        `;
       } else {
         alert = `
           <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <strong>${response.error}</strong>
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
-        `
+        `;
       }
-      $(alert).insertBefore('#edicao')
-    }
-  })
+      $(alert).insertBefore("#edicao");
+    },
+  });
 }
-
 
 function remover_tema(_id) {
   $.ajax({
-    type: 'DELETE',
+    type: "DELETE",
     url: `${url_base}delete_theme/${_id}`,
     success: function () {
-      window.location.href = `http://localhost:5000`
-    }
-  })
+      window.location.href = `http://localhost:3000`;
+    },
+  });
 }
 
-
-function salvar_pontuacao (_id, pontuacao, erros, tempo) {
-  let req_obj = {}
-  req_obj.theme_id = _id
-  req_obj.score = pontuacao
-  req_obj.mistakes = erros
-  req_obj.time = tempo
+function salvar_pontuacao(_id, pontuacao, erros, tempo) {
+  let req_obj = {};
+  req_obj.theme_id = _id;
+  req_obj.score = pontuacao;
+  req_obj.mistakes = erros;
+  req_obj.time = tempo;
 
   $.ajax({
-    type: 'POST',
+    type: "POST",
     url: `${url_base}save_score`,
     data: JSON.stringify(req_obj),
-    contentType: 'application/json; charset=utf-8',
-    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
     success: function () {
-      console.log('Pontuação salva com sucesso!')
-    }
-  })
+      console.log("Pontuação salva com sucesso!");
+    },
+  });
 }
 
+function check_senha(theme_password, theme_id) {
+  let password = $(`#${theme_id}-password`).val();
 
-function check_senha (theme_password, theme_id) {
-  let password = $(`#${theme_id}-password`).val()
-
-  console.log(theme_password, password)
+  console.log(theme_password, password);
 
   if (password === theme_password) {
-    window.location.href = `http://localhost:5000/tema/${theme_id}`
+    window.location.href = `http://localhost:3000/tema/${theme_id}`;
   } else {
     let alert = `
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
           <strong>Senha errada!</strong>
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-      `
-    $(alert).insertBefore('#themes')
+      `;
+    $(alert).insertBefore("#themes");
   }
 }
